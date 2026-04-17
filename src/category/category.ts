a9249af1bdfd0d9c4b87fab79cb786371a1e33e9
Joan Santamans Perez <joansantamansperez@gmail.com>
2026-04-17 13:31:23 +0200

    Listado simple finalizado

--- src/category/category.ts
+++ src/category/category.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CATEGORY_DATA } from './model/mock-categories';
 import { Category } from './model/category';
@@ -10,17 +11,23 @@ import { Observable, of } from 'rxjs';
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private baseUrl = 'http://localhost:8080/category';
 
   getCategories(): Observable<Category[]> {
-    return of(CATEGORY_DATA);
+    return this.http.get<Category[]>(this.baseUrl);
   }
 
   saveCategory(category: Category): Observable<Category> {
-    return of(null);
+    const { id } = category;
+    const url = id ? `${this.baseUrl}/${id}` : this.baseUrl;
+    return this.http.put<Category>(url, category);
   }
 
   deleteCategory(idCategory : number): Observable<any> {
-    return of(null);
+    return this.http.delete(`${this.baseUrl}/${idCategory}`);
   }  
 }
