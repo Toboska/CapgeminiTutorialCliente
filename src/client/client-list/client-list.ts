fee3bd1ef5a31b0152dcbe156f2b691f8b43d3d6
Joan Santamans Perez <joansantamansperez@gmail.com>
2026-04-22 13:53:11 +0200

    Front

--- src/client/client-list/client-list.ts
+++ src/client/client-list/client-list.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Client } from '../model/Client';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ClientService } from '../client';
+import { MatDialog } from '@angular/material/dialog';
+import { ClientEdit } from '../client-edit/client-edit';
+import { DialogConfirmation } from '../../app/core/dialog-confirmation/dialog-confirmation';
+
+//El .ts se encarga de definit la lógica, comportamiento, y se conecta con su html y estilos(scss)
 
 @Component({
   selector: 'app-client-list',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    CommonModule
+  ],
   templateUrl: './client-list.html',
   styleUrl: './client-list.scss',
 })
-export class ClientList {}
+
+//OnInit interfaz de angular para cargar datos, llamar a servicios y inicializar variables
+export class ClientList implements OnInit{ 
+  
+  //MatTableDataSource clase de Angular que permite gestionar los datos de una tabla y facilita paginación...
+  dataSource = new MatTableDataSource<Client>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  constructor(
+    private clientService = ClientService,
+    public dialog: MatDialog,
+  ){}
+
+  ngOnInit(): void {
+    
+  }
+
+
+}
